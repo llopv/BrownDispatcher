5439562c5243efa5cf43754fe4c9be21e8b70d9e
David Llop <llop-no-spam-please@protonmail.com>
2018-08-27 13:12:19 +0200

    Handle error when no config file

--- BrownDispatcher.py
+++ BrownDispatcher.py
@@ -1,7 +1,12 @@
 import configparser
+import sys
+
 config = configparser.ConfigParser()
 config.read("config")
-user = config['Credentials']['user']
-password = config['Credentials']['pass']
-
+try:
+  user = config['Credentials']['user']
+  password = config['Credentials']['pass']
+except KeyError:
+  print "Config file needed. Plese, copy and fill config-default."
+  sys.exit()
 print(user, password)
