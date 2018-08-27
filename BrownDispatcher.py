d9c612dc14c1ab67e600ed7b39aa1ad8f9f69d49
David Llop <llop-no-spam-please@protonmail.com>
2018-08-27 13:35:38 +0200

    Fix utf8 problem

--- BrownDispatcher.py
+++ BrownDispatcher.py
@@ -1,5 +1,6 @@
 import configparser
 import sys
+from github import Github
 
 config = configparser.ConfigParser()
 config.read("config")
@@ -7,6 +8,11 @@ try:
   user = config['Credentials']['user']
   password = config['Credentials']['pass']
 except KeyError:
-  print "Config file needed. Plese, copy and fill config-default."
+  print("Config file needed. Plese, copy and fill config-default.")
   sys.exit()
 print(user, password)
+
+g = Github(user, password)
+
+for repo in g.get_user().get_repos():
+  print(repo.name)
